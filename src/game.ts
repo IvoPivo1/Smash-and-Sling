44137bdceb08a73eed31c0574a77606c71d88b42
IvoPivo1 <ivogjurovski12@gmail.com>
2026-02-03 13:52:18 +0100

    Created a player that can move around with arrow keys and jump with space

--- src/game.ts
+++ src/game.ts
@@ -1,50 +1,18 @@
 class Game {
-  private position: p5.Vector;
-  private isCircleVisible: boolean;
+  private player: Player;
 
   constructor() {
-    this.position = createVector(width * 0.5, height * 0.5);
-    this.isCircleVisible = false;
+    this.player = new Player();
   }
 
   public update() {
-    this.position.set(mouseX, mouseY);
-    this.isCircleVisible = mouseIsPressed;
-
-    if (mouseIsPressed) {
-      if (!music.mystery.isPlaying()) {
-        music.mystery.loop();
-      }
-    } else {
-      music.mystery.pause();
-    }
+    this.player.update();
   }
+    
 
   public draw() {
-    background("black");
-    this.drawText();
-
-    if (this.isCircleVisible) {
-      this.drawCircle();
-    }
+    background(0);
+    this.player.draw();
   }
 
-  public drawText() {
-    push();
-    fill("white");
-    textSize(width * 0.1);
-    textStyle("bold");
-    textAlign("center");
-    text("Click & Drag", width * 0.5, height * 0.5);
-    pop();
-  }
-
-  public drawCircle() {
-    push();
-    fill(0, 255, 0, 200);
-    stroke("white");
-    strokeWeight(width * 0.01);
-    circle(this.position.x, this.position.y, width * 0.2);
-    pop();
-  }
 }
---
+++ src/player.ts
@@ -0,0 +1,52 @@
+class Player {
+  private position: p5.Vector;
+  private size: p5.Vector;
+  private velocity: p5.Vector;
+
+  constructor() {
+    this.position = createVector(width / 2, height / 2);
+    this.size = createVector(50, 80);
+    this.velocity = createVector(0, 0);
+  }
+
+  public update() {
+    this.applyGravity();
+    this.updatePosition();
+    this.move();
+  }
+
+  private move() {
+    if (keyIsDown(LEFT_ARROW)) {
+      this.position.x -= 5;
+    }else if (keyIsDown(RIGHT_ARROW)) {
+      this.position.x += 5;
+    } else{
+        this.velocity.x = 0;
+    }
+
+    if (keyIsDown(32) && this.position.y >= height - 100) {
+      this.velocity.y = -20;
+    }
+  }
+
+
+  private updatePosition() {
+    this.position.add(this.velocity);
+    if (this.position.y > height -100) {
+      this.velocity.y = 0;
+      this.position.y = height -100;
+    }
+  }
+
+  private applyGravity() {
+    this.velocity.y += 0.8; // Gravity acceleration
+  }
+
+  public draw() {
+    push();
+    fill("white");
+    rect(this.position.x, this.position.y, this.size.x, this.size.y);
+
+    pop();
+  }
+}
