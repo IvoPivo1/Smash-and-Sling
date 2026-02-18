e4a6e42ac48fbaa8e251e883a87942250c9753dc
AbdirahmanWarsame <abdir.w99@gmail.com>
2026-02-18 07:04:24 +0100

    new floor and changed size

--- src/game.ts
+++ src/game.ts
@@ -1,20 +1,16 @@
 class Game {
-  private player: Player;
   private level1: Level1;
 
   constructor() {
-    this.player = new Player();
     this.level1 = new Level1();
   }
 
   public update() {
-    this.player.update();
     this.level1.update();
   }
 
   public draw() {
     background(135, 206, 235);
     this.level1.draw();
-    this.player.draw();
   }
 }
--- src/level1.ts
+++ src/level1.ts
@@ -1,40 +1,46 @@
 class Level1 {
   public update() {}
 
-  // ritar banan varje frame
   public draw() {
     push();
     stroke(0);
     strokeWeight(2);
 
     fill(255);
-    // bilder pig
-    image(images.pig, 938, height - 220 - 40, 40, 40);
+    // image pig
+    image(images.pig, 938, height - 200 - 40, 40, 40);
 
-    image(images.pig, 1038, height - 220 - 40, 40, 40);
+    image(images.pig, 1038, height - 200 - 40, 40, 40);
 
-    image(images.pig, 1138, height - 320 - 40, 40, 40);
+    image(images.pig, 1138, height - 300 - 40, 40, 40);
 
-    image(images.pig, 938, height - 432 - 40, 40, 40);
+    image(images.pig, 938, height - 333 - 40, 40, 40);
 
-    // stolpar
-    rect(900, height - 220, 12, 220);
+    // poles
+    rect(900, height - 200, 12, 200);
 
-    rect(1000, height - 220, 12, 220);
+    rect(1000, height - 200, 12, 200);
 
-    rect(1100, height - 320, 12, 320);
+    rect(1100, height - 300, 12, 300);
 
-    rect(1200, height - 320, 12, 320);
+    rect(1200, height - 300, 12, 300);
 
-    // top pinnar
-    rect(900, height - 220, 212, 12);
-    rect(1100, height - 320, 112, 12);
+    // top poles
+    rect(900, height - 200, 212, 12);
+    rect(1100, height - 300, 112, 12);
 
-    // andra våningen stolpar
-    rect(900, height - 210 - 12 - 210, 12, 210);
-    rect(1000, height - 210 - 12 - 210, 12, 210);
+    // second floor poles
+    rect(900, height - 154 - 12 - 154, 12, 120);
+    rect(1000, height - 154 - 12 - 154, 12, 120);
 
-    // andra våningen top pinne
+    // second floor top pole
+    rect(900, height - 160 - 12 - 160, 112, 12);
+
+    // third floor pole
+    rect(900, height - 210 - 12 - 210, 12, 100);
+    rect(1000, height - 210 - 12 - 210, 12, 100);
+
+    // third floor top pole
     rect(900, height - 210 - 12 - 210, 112, 12);
 
     pop();
