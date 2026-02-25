afdecfda7934b22a75ce0ce94dd02063f745f0b0
AbdirahmanWarsame <abdir.w99@gmail.com>
2026-02-25 06:57:53 +0100

    change pole to center coordinate system

--- src/pole.ts
+++ src/pole.ts
@@ -2,7 +2,9 @@
 
 class Pole extends Entity {
   constructor(x: number, y: number, width: number, height: number) {
-    super(undefined, x, y, width, height);
+    const cx = x + width / 2;
+    const cy = y + height / 2;
+    super(undefined, cx, cy, width, height);
   }
 
   public onCollision(other: Entity): void {}
