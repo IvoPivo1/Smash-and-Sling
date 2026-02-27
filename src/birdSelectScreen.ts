d4df2d6f9e79033a1e4687cc51e2bf1c5a0a2f9d
AbdirahmanWarsame <abdir.w99@gmail.com>
2026-02-27 09:26:59 +0100

    start level with empty entities array

--- src/birdSelectScreen.ts
+++ src/birdSelectScreen.ts
@@ -70,7 +70,7 @@ class BirdSelectScreen {
 
         game.selectedBird = bird.id;
         game.selectedSprite = bird.sprite;
-        game.level = new Level();
+        game.level = new Level([]);
         game.state = "level";
       }
     }
