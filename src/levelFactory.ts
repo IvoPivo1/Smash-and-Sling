6a03ff6af7b407a72cc596f828c7d25e3fbafb3e
AbdirahmanWarsame <abdir.w99@gmail.com>
2026-03-03 11:09:48 +0100

    some initial level design

--- src/levelFactory.ts
+++ src/levelFactory.ts
@@ -11,12 +11,30 @@ class LevelFactory {
         return this.levelThree();
       case 4:
         return this.levelFour();
-
+      /*
       case 5:
         return this.levelFive();
     }
+    
+     case 6:
+        return this.levelSix();
+    }
+     case 7:
+        return this.levelFive();
+    }
+     case 8:
+        return this.levelFive();
+    }
+     case 9:
+        return this.levelFive();
+    }
+     case 10:
+        return this.levelFive();
+    }*/
+    }
     return this.levelOne();
   }
+
   // level 1
   private levelOne(): Level {
     // bygger listan med entities för leveln
@@ -27,7 +45,7 @@ class LevelFactory {
       new Pig(955, 260, random(pigList)),
 
       // x, y, width, height
-      new BlackPole(900, 550, 12, 500, 0),
+      new Pole(900, 550, 12, 500, 0),
       new Pole(1000, 550, 12, 500, 0),
       new Pole(900, 550, 112, 12),
     ];
@@ -40,14 +58,15 @@ class LevelFactory {
     const entities: Entity[] = [
       new Player(game.selectedBirdObject!),
 
-      new Pig(955, 260, random(pigList)),
+      //   new Pig(955, 260, random(pigList)),
       new Pig(955, 580, random(pigList)),
 
-      new Pole(900, 500, 12, 500, 0),
+      new BlackPole(900, 600, 12, 500, 0),
+      new Pole(900, 500, 12, 100, 0),
       new Pole(1000, 500, 12, 500, 0),
+
       new Pole(900, 600, 112, 12),
-      new Pole(900, 800, 12, 500),
-      new Pole(1000, 800, 12, 500),
+
       new Pole(900, 500, 112, 12),
     ];
 
@@ -96,36 +115,6 @@ class LevelFactory {
       new Pole(900, 580, 12, 120),
       new Pole(1000, 580, 12, 120),
       new Pole(900, 567, 112, 12),
-    ];
-
-    return new Level(entities, 0);
-  }
-
-  // level 5
-  private levelFive(): Level {
-    const entities: Entity[] = [
-      new Player(game.selectedBirdObject!),
-
-      new Pig(850, 680, random(pigList)),
-      new Pig(850, 600, random(pigList)),
-
-      new Pole(820, 700, 12, 200, 0),
-      new Pole(880, 700, 12, 200, 0),
-      new Pole(820, 700, 72, 12),
-
-      new Pole(820, 620, 12, 200, 0),
-      new Pole(880, 620, 12, 200, 0),
-      new Pole(820, 620, 72, 12),
-
-      new Pig(1150, 680, random(pigList)),
-      new Pig(1150, 600, random(pigList)),
-      new Pole(1120, 700, 12, 200, 0),
-      new Pole(1180, 700, 12, 200, 0),
-      new Pole(1120, 700, 72, 12),
-
-      new Pole(1120, 620, 12, 200, 0),
-      new Pole(1180, 620, 12, 200, 0),
-      new Pole(1120, 620, 72, 12),
     ];
 
     return new Level(entities, 0);
